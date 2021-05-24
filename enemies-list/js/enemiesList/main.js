d4115ce8346204aba17b3172677ff6cac0f65a54
NSPatton <greengun15@gmail.com>
2021-05-24 14:49:26 -0500

    np-placenames

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,10 @@
 // Put your code here
+
+for (const myEnemy in enemies) {
+    if (myEnemy.isReallyHated) {
+        console.log(`${myEnemy.firstName} ${myEnemy.LastName} Really, really dislike!`)
+    }
+    else {
+        console.log(`${myEnemy.FirstName} ${myEnemy.LastName}`)
+    }
+}
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,18 @@
 // Put your code here
+const names = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+]
+
+console.log("All Place Names")
+
+for (let name of names) {
+    console.log(name)
+}
+
+
+let theNames = names.filter(name => name.includes("The"))
+
+for (let name of theNames) {
+    console.log(name)
+}
